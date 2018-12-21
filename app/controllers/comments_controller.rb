80e0063ef82dd12406e99128826635e16e3bf83c
cool-beetles <adrianna.kucinska@onet.eu>
2018-12-21 11:28:11 +0100

    Add actions in comments controleer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,29 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   
   def index
+    @comments = Comment.movie.all	
   end
 
   def new
+  	@comment = Comment.new
   end
 
   def create
+    @movie = Movie.find(params[:movie_id])
+    @comment = @movie.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    
+    if @comment.save
+      redirect_to movie_path 
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:body, :post_id, user_attributes: [:name, :email])
+  end
   end
 end
