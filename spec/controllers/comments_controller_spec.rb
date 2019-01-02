e58ac735e43ea07ecb1e7bbe74f1b02b2d7dd8c0
cool-beetles <adrianna.kucinska@onet.eu>
2019-01-02 21:31:37 +0100

    Add tests for comments controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  describe 'as logged user' do
+    let(:genre) { Genre.create(name: 'comedy') }
+    let(:movie) { Movie.create(title: 'Test Wars', description: "Test 123 test", genre: genre) }
+    let(:user)  { User.create(id: 22, name: 'Test', email: 'test@test.com', password: 'secret' ) }
+    
+    before do
+      sign_in(user)
+      user.confirm
+    end
 
+    describe 'POST create' do
+      it 'should save the new comment in the database' do
+        expect{ post :create, params: { movie_id: movie.id, comment: { body: 'test 123' } } }.to change{Comment.count}.by(1)
+      end
+      
+      context "create comment with invalid attributes" do
+        it "should not save the new comment in the database" do
+          expect{ post :create, params: { movie_id: movie.id, comment: { body: '' } } }.to change{Comment.count}.by(0)
+        end
+      end
+    end
+
+    # I have a problem with this test, so I add like comment
+    
+    # describe 'DELETE destroy' do      
+    #   it 'delete useless comment' do
+    #     comment = user.comments.first
+    #     delete :destroy, params: { movie_id: movie.id, comment: { id: comment.id } }
+    #     expect(response).to redirect_to(movie_path(comment.movie_id))
+    #   end
+    # end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= "test"
 require "spec_helper"
 require File.expand_path("../config/environment", __dir__)
 require "rspec/rails"
+require "devise"
 require "pry"
 require "capybara/rails"
 require "simplecov"
@@ -34,6 +35,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
   config.include Capybara::DSL
+  config.include Devise::Test::ControllerHelpers, type: :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
